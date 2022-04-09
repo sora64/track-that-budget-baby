a3b59c625db4ae55a4861c1add9a23d87c4eeb62
Alan Phelps <phelpsa64@gmail.com>
2022-04-09 12:10:22 -0400

    updated service worker FILES_TO_CACHE

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,11 +2,17 @@ const APP_PREFIX = "BudgetTracker-";
 const VERSION = "version_01";
 const CACHE_NAME = APP_PREFIX + VERSION;
 const FILES_TO_CACHE = [
-  "./public/index.html",
-  "./public/icons/",
-  "./models/transaction.js",
-  "./routes/api.js",
-  "./server.js"
+  "./index.html",
+  "./icons/icon-72x72.png",
+  "./icons/icon-96x96.png",
+  "./icons/icon-128x128.png",
+  "./icons/icon-144x144.png",
+  "./icons/icon-152x152.png",
+  "./icons/icon-192x192.png",
+  "./icons/icon-384x384.png",
+  "./icons/icon-512x512.png",
+  "./js/idb.js",
+  "./js/index.js"
 ];
 
 // Respond with cached resources
